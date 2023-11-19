05f7d4213b94256f30842683eb2f561f9045ac95
lanschekov <egorrkaboxer@gmail.com>
2023-11-19 16:33:52 +0300

    this commit amends the last commit made on GitHub.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Another line')
+    print('Some string from GitHub (Correctly)')
 
 
 if __name__ == '__main__':
