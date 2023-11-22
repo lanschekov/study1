5cc46556eb3b5ecc4e82d41b65f3250b20dc761b
Egor Lanschekov <egorrkaboxer@gmail.com>
2023-11-22 06:14:05 +0300

    new function my_superfunction

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Another line')
-    print('Some string from GitHub (Correctly)')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
